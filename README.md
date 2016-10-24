b24c3dabc2cbdec41440e9b2f12e2c3c97f30a3e
Shi Ping <limshiping93@hotmail.com>
2016-10-24 23:19:10 +0800

    General: refactored project name

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 SUTD 50.008 Database Project
 
 ## Project brief
-B&N is a large bookstore and it has decided to go online. Your team has been called in to help B&N design and implement the prototype of its online shop.
+B&N is a large db-project and it has decided to go online. Your team has been called in to help B&N design and implement the prototype of its online shop.
 
 ## Setup
 1. Install pip (package management system)
--- db-project/db-project/wsgi.py
+++ db-project/db-project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bookstore project.
+WSGI config for db-project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "db-project.settings")
 
 application = get_wsgi_application()
