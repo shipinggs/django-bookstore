ba97313f1b029713d09a4b500f0e4f52b509545d
Shi Ping <limshiping93@hotmail.com>
2016-11-07 00:37:48 +0800

    Settings: updated settings for mysql usage

--- db-project/db-project/settings.py
+++ db-project/db-project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'db-project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Singapore'
 
 USE_I18N = True
 
