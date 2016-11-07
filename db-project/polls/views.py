6c5b280848ee82bbb4d1cf1e5e318c21912232db
Shi Ping <limshiping93@hotmail.com>
2016-11-07 12:28:52 +0800

    Polls: set view's content to be based on db

--- db-project/polls/views.py
+++ db-project/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello world. You're at the polls page.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
