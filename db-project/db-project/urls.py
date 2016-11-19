848b7ea3993b8dd90f8fc17cde0b3f6344b8fc72
dansyuqri <d.syuqri@hotmail.com>
2016-11-19 21:37:08 +0800

    added bookstore configs into project settings and urls files

--- db-project/db-project/urls.py
+++ db-project/db-project/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^bookstore/', include('bookstore.urls')),
 ]
