80bf3b0e2a8c991d47df78b5a827ab9b17176011
dansyuqri <d.syuqri@hotmail.com>
2016-11-21 12:55:48 +0800

    added functions to the different pages in views.py
    account()
    admin()
    cart()
    home()
    index()
    register()

--- db-project/bookstore/views.py
+++ db-project/bookstore/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(request):
+def account(request):
+    #TODO: add functionality
+    return HttpResponse('<h1>You are at account page</h1>')
+
+def admin(request):
+    #TODO: add functionality
+    return HttpResponse('<h1>You are at admin page</h1>')
+
+def cart(request):
+    #TODO: add functionality
+    return HttpResponse('<h1>You are at cart page</h1>')
+
+def home(request):
+    #TODO: add functionality
     return render(request, 'bookstore/login.html')
+
+def index(request):
+    #TODO: either remove or add functionality here
+    return HttpResponse('<h1>You are at account page</h1>')
+
+def register(request):
+    #TODO: add functionality
+    return HttpResponse('<h1>You are at register page</h1>')
