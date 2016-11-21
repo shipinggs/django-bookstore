e7e19177e2608066010f00e3cab554c0568238a2
dansyuqri <d.syuqri@hotmail.com>
2016-11-21 12:55:17 +0800

    added urls to urls.py
    account
    admin
    cart
    home
    index
    register

--- db-project/bookstore/urls.py
+++ db-project/bookstore/urls.py
@@ -3,5 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'account', views.account, name='account'),
+    url(r'admin', views.admin, name='admin'),
+    url(r'cart', views.cart, name='cart'),
+    url(r'home', views.home, name='home'),
     url(r'^$', views.index, name='index'),
+    url(r'register', views.register, name='register'),
 ]
