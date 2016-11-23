94b230c137fd44931a74e3543408b824458660a2
wh000 <weihan_sang93@hotmail.com>
2016-11-23 20:13:30 +0800

    fixed wrong url

--- db-project/bookstore/urls.py
+++ db-project/bookstore/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     url(r'^home$', views.home, name='home'),
     url(r'^login$', views.login, name='login'),
     # url(r'^$', views.index, name='index'),
-    url(r'^query$', views.query, name='query'),
     url(r'^register$', views.register, name='register'),
     url(r'^book/(?P<bid>[a-zA-Z0-9]+)$', views.book_details, name = 'book_details'),
  	url(r'^book/(?P<bid>[a-zA-Z0-9]+)/add-to-cart$', views.add_to_cart, name = 'add_to_cart'),
