3a5a9bd929d7f35e9f9db7c1b66277b3cf5ded7a
dansyuqri <d.syuqri@hotmail.com>
2016-11-30 12:59:19 +0800

    added saving of session requests

--- db-project/db-project/settings.py
+++ db-project/db-project/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-
+SESSION_SAVE_EVERY_REQUEST = True
 # Login redirect
 LOGIN_URL = 'bookstore:login'
